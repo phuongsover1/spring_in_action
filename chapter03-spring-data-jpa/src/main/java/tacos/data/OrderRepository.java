05371887d416dd5dd2ff93b873b5fbe96a7657f4
Phuong Nguyen <phuongsover2@gmail.com>
2023-12-31 11:32:40 +0700

    Update @Query

--- chapter03-spring-data-jpa/src/main/java/tacos/data/OrderRepository.java
+++ chapter03-spring-data-jpa/src/main/java/tacos/data/OrderRepository.java
@@ -3,6 +3,7 @@ package tacos.data;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import tacos.domain.TacoOrder;
@@ -13,7 +14,7 @@ public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
     
     List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
 
-    // @Query("Order o where o.deliveryCity='Seattle'")
-    // List<TacoOrder> readOrdersDeliveredInSeattle();
+    @Query("SELECT o FROM TacoOrder o where o.deliveryCity='Seattle'")
+    List<TacoOrder> readOrdersDeliveredInSeattle();
 
 }
