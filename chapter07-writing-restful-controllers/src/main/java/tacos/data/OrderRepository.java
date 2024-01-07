99cb6afb78a103d73199479e2a833d86567b91fb
Phuong Nguyen <phuongsover2@gmail.com>
2024-01-07 23:51:55 +0700

    Init project

---
+++ chapter07-writing-restful-controllers/src/main/java/tacos/data/OrderRepository.java
@@ -0,0 +1,24 @@
+package tacos.data;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import tacos.User;
+import tacos.domain.TacoOrder;
+
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+    
+    List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+
+    @Query("SELECT o FROM TacoOrder o where o.deliveryCity='Seattle'")
+    List<TacoOrder> readOrdersDeliveredInSeattle();
+
+    List<TacoOrder> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
+
+}
