f3ee9a2522875f7101223eacba77396aa010cf73
Phuong Nguyen <phuongsover2@gmail.com>
2024-01-12 22:49:14 +0700

    7.3.2 PUTting resources to update ingredient from Spring Application.

--- chapter07-enabling-data-services/src/main/java/tacos/consuming_rest/ConsumingIngredientController.java
+++ chapter07-enabling-data-services/src/main/java/tacos/consuming_rest/ConsumingIngredientController.java
@@ -4,10 +4,7 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -24,7 +21,7 @@ import java.util.Map;
 public class ConsumingIngredientController {
   private final RestTemplate restTemplate;
 
-  @GetMapping("/{id}")
+  @GetMapping("/getForObject/{id}")
   public ResponseEntity<Ingredient> getIngredientById(@PathVariable String id) {
     Map<String, String> urlVariables = new HashMap<>();
     urlVariables.put("id", id);
@@ -38,6 +35,22 @@ public class ConsumingIngredientController {
     } catch (RestClientException ex) {
     }
     return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+  }
+
+  @GetMapping("/getForEntity/{id}")
+  public Ingredient getIngredientByIdUsingGetForEntity(@PathVariable String id) {
+
+    ResponseEntity<Ingredient> responseEntity = restTemplate.getForEntity("http://localhost:8080/data-api/ingredients/{id}",
+            Ingredient.class,
+            id);
+    log.info("Fetched time: {}", responseEntity.getHeaders().getDate());
+    return responseEntity.getBody();
+
+  }
+
+  @PutMapping(value = "/{id}", consumes = "application/json")
+  public void updateIngredientUsingPut(@PathVariable String id, @RequestBody Ingredient ingredient) {
+    restTemplate.put("http://localhost:8080/data-api/ingredients/{id}", ingredient, id);
   }
 
 }
