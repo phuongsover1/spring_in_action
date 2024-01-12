d8f8c214e060536d54fe01cd0a7f6a750c1d0b34
Phuong Nguyen <phuongsover2@gmail.com>
2024-01-13 01:48:39 +0700

    8.2 Creating an authorization server

---
+++ chapter08-spring-authorization-server/src/main/java/com/tacocloud/authorizationserver/Chapter08SpringAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.tacocloud.authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter08SpringAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter08SpringAuthorizationServerApplication.class, args);
+	}
+
+}
