129ec98803346c1c8146593721fa226cb824989c
Phuong Nguyen <phuongsover2@gmail.com>
2024-01-14 18:46:36 +0700

    8.3 Securing an API with a resource server

--- chapter05-securing-spring/src/main/java/tacos/User.java
+++ chapter05-securing-spring/src/main/java/tacos/User.java
@@ -37,6 +37,7 @@ public class User implements UserDetails {
     private final String zip;
     private final String phoneNumber;
 
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
