92ac9c73e6db841188e788c158d9432729f35127
Phuong Nguyen <phuongsover2@gmail.com>
2024-01-14 18:46:10 +0700

    8.2 Creating an authorization server

--- chapter08-spring-authorization-server/src/main/java/com/tacocloud/authorizationserver/Chapter08SpringAuthorizationServerApplication.java
+++ chapter08-spring-authorization-server/src/main/java/com/tacocloud/authorizationserver/Chapter08SpringAuthorizationServerApplication.java
@@ -1,13 +1,24 @@
 package com.tacocloud.authorizationserver;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Chapter08SpringAuthorizationServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Chapter08SpringAuthorizationServerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Chapter08SpringAuthorizationServerApplication.class, args);
+  }
+
+  @Bean
+  public ApplicationRunner dataLoader(UserRepository userRepo, PasswordEncoder passwordEncoder) {
+    return args -> {
+      userRepo.save(new User("habuma", passwordEncoder.encode("password"), "ROLE_ADMIN" ));
+      userRepo.save(new User("tacochef", passwordEncoder.encode("password"), "ROLE_ADMIN"));
+    };
+  }
 
 }
