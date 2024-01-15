9a5e6700a58be924de73feea2a5838f54d33e3a4
Phuong Nguyen <phuongsover2@gmail.com>
2024-01-15 23:08:45 +0700

    Init project

---
+++ chapter09-asynchronous-messaging-JMS/src/main/java/tacos/services/OrderAdminService.java
@@ -0,0 +1,5 @@
+package tacos.services;
+
+public interface OrderAdminService {
+  void deleteAllOrders();
+}
---
+++ chapter09-asynchronous-messaging-JMS/src/main/java/tacos/web/api/OrderRestController.java
@@ -0,0 +1,64 @@
+package tacos.web.api;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tacos.data.OrderRepository;
+import tacos.domain.TacoOrder;
+
+import java.util.Optional;
+
+@Slf4j
+@Data
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping(path = "/api/orders", produces = "application/json")
+public class OrderRestController {
+  private final OrderRepository orderRepo;
+
+  @PutMapping(path = "/{orderId}", consumes = "application/json")
+  public TacoOrder putOrder(@PathVariable("orderId") Long orderId, @RequestBody TacoOrder order) {
+    order.setId(orderId);
+    return orderRepo.save(order);
+  }
+
+  @GetMapping("/{orderId}")
+  public ResponseEntity<TacoOrder> orderById(@PathVariable("orderId") Long orderId) {
+    Optional<TacoOrder> optOrder = orderRepo.findById(orderId);
+    return optOrder.map(order -> new ResponseEntity<>(order, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
+  }
+
+  @PatchMapping(path = "/{orderId}", consumes = "application/json")
+  public TacoOrder patchOrder(@PathVariable("orderId") Long orderId, @RequestBody TacoOrder patch) {
+    TacoOrder order = orderRepo.findById(orderId).get();
+    if (patch.getDeliveryName() != null)
+      order.setDeliveryName((patch.getDeliveryName()));
+    if (patch.getDeliveryCity() != null)
+      order.setDeliveryCity(patch.getDeliveryCity());
+    if (patch.getDeliveryState() != null)
+      order.setDeliveryState(patch.getDeliveryState());
+    if (patch.getDeliveryZip() != null)
+      order.setDeliveryZip(patch.getDeliveryZip());
+    if (patch.getCcNumber() != null)
+      order.setCcNumber(patch.getCcNumber());
+    if (patch.getCcExpiration() != null)
+      order.setCcExpiration(patch.getCcExpiration());
+    if (patch.getCcCVV() != null)
+      order.setCcCVV(patch.getCcCVV());
+    return orderRepo.save(order);
+  }
+
+  @DeleteMapping("/{orderId}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteOrder(@PathVariable("orderId")Long id) {
+    try {
+      orderRepo.deleteById(id);
+    } catch (EmptyResultDataAccessException e) {
+
+    }
+
+  }
+}
