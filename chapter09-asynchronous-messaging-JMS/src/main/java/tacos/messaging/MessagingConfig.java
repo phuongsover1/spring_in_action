1504742d199c3fa1a569135eb470c1f742b05ec8
Phuong Nguyen <phuongsover2@gmail.com>
2024-01-16 23:43:34 +0700

    9.1.2 Specifying type of the incoming object being sent to the server

--- chapter09-asynchronous-messaging-JMS/src/main/java/tacos/messaging/MessagingConfig.java
+++ chapter09-asynchronous-messaging-JMS/src/main/java/tacos/messaging/MessagingConfig.java
@@ -4,6 +4,11 @@ import jakarta.jms.Destination;
 import org.apache.activemq.artemis.jms.client.ActiveMQQueue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import tacos.domain.TacoOrder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class MessagingConfig {
@@ -11,6 +16,14 @@ public class MessagingConfig {
   @Bean
   public Destination customQueue() {
     return new ActiveMQQueue("tacocloud.custom.queue");
+  }
+
+  // Define a message converter to convert message to and from json
+  @Bean
+  public MappingJackson2MessageConverter messageConverter() {
+    MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
+    messageConverter.setTypeIdPropertyName("_typeId");;
+    return messageConverter;
   }
 }
 
