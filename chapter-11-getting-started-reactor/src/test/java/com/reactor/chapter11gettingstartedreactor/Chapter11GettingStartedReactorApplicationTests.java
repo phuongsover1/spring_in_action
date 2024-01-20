6efeb3794c5696c7f74fce209818d3a040024949
Phuong Nguyen <phuongsover2@gmail.com>
2024-01-21 00:16:00 +0700

    Use StepVerify to test the order of item in FLUX

--- chapter-11-getting-started-reactor/src/test/java/com/reactor/chapter11gettingstartedreactor/Chapter11GettingStartedReactorApplicationTests.java
+++ chapter-11-getting-started-reactor/src/test/java/com/reactor/chapter11gettingstartedreactor/Chapter11GettingStartedReactorApplicationTests.java
@@ -3,6 +3,7 @@ package com.reactor.chapter11gettingstartedreactor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 import java.util.function.Consumer;
 
@@ -17,7 +18,15 @@ class Chapter11GettingStartedReactorApplicationTests {
 	void createAFlux_just() {
 		Flux<String> fruitFlux = Flux.just("Apple", "Orange", "Grape", "Banana", "Strawberry");
 
-		fruitFlux.subscribe(s -> System.out.println("Here's some fruit: " + s));
+//		fruitFlux.subscribe(s -> System.out.println("Here's some fruit: " + s));
+		StepVerifier.create(fruitFlux)
+				.expectNext("Apple")
+				.expectNext("Orange")
+				.expectNext("Grape")
+				.expectNext("Banana")
+				.expectNext("Strawberry")
+				.verifyComplete();
+
 	}
 
 }
