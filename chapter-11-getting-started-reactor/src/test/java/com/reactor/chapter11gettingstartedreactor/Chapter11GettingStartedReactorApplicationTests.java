8af49c8bf3267dae81adb5714bb16d5433b24a46
Phuong Nguyen <phuongsover2@gmail.com>
2024-01-21 00:09:34 +0700

    create a Flux and subscribe it to Consumer

--- chapter-11-getting-started-reactor/src/test/java/com/reactor/chapter11gettingstartedreactor/Chapter11GettingStartedReactorApplicationTests.java
+++ chapter-11-getting-started-reactor/src/test/java/com/reactor/chapter11gettingstartedreactor/Chapter11GettingStartedReactorApplicationTests.java
@@ -2,12 +2,22 @@ package com.reactor.chapter11gettingstartedreactor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Consumer;
 
 @SpringBootTest
 class Chapter11GettingStartedReactorApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void createAFlux_just() {
+		Flux<String> fruitFlux = Flux.just("Apple", "Orange", "Grape", "Banana", "Strawberry");
+
+		fruitFlux.subscribe(s -> System.out.println("Here's some fruit: " + s));
 	}
 
 }
