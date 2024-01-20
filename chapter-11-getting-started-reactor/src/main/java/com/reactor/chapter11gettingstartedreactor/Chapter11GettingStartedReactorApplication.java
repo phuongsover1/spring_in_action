ee42b3d3e1f0bb9c6856617d7c89774a86cb67ab
Phuong Nguyen <phuongsover2@gmail.com>
2024-01-20 23:57:09 +0700

    init project

---
+++ chapter-11-getting-started-reactor/src/main/java/com/reactor/chapter11gettingstartedreactor/Chapter11GettingStartedReactorApplication.java
@@ -0,0 +1,13 @@
+package com.reactor.chapter11gettingstartedreactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter11GettingStartedReactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter11GettingStartedReactorApplication.class, args);
+	}
+
+}
---
+++ chapter-11-getting-started-reactor/src/test/java/com/reactor/chapter11gettingstartedreactor/Chapter11GettingStartedReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactor.chapter11gettingstartedreactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter11GettingStartedReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
