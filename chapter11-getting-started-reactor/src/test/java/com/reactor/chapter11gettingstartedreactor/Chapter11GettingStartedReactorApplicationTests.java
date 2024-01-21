be4a71ade22624babe0149d27fc78f89e20fedc3
Phuong Nguyen <phuongsover2@gmail.com>
2024-01-21 14:18:05 +0700

    Flux.fromArray, Flux.fromIterable, Flux.fromStream. merged Fluxes

---
+++ chapter11-getting-started-reactor/src/test/java/com/reactor/chapter11gettingstartedreactor/Chapter11GettingStartedReactorApplicationTests.java
@@ -0,0 +1,137 @@
+package com.reactor.chapter11gettingstartedreactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+@SpringBootTest
+class Chapter11GettingStartedReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void createAFlux_just() {
+		Flux<String> fruitFlux = Flux.just("Apple", "Orange", "Grape", "Banana", "Strawberry");
+
+//		fruitFlux.subscribe(s -> System.out.println("Here's some fruit: " + s));
+		StepVerifier.create(fruitFlux)
+				.expectNext("Apple")
+				.expectNext("Orange")
+				.expectNext("Grape")
+				.expectNext("Banana")
+				.expectNext("Strawberry")
+				.verifyComplete();
+
+	}
+
+	@Test
+	void createAFlux_fromArray() {
+		String[] fruits = new String[] {"Apple", "Orange", "Grape", "Banana", "Strawberry"};
+
+		Flux<String> fruitFlux = Flux.fromArray(fruits);
+
+		StepVerifier.create(fruitFlux)
+				.expectNext("Apple")
+				.expectNext("Orange")
+				.expectNext("Grape")
+				.expectNext("Banana")
+				.expectNext("Strawberry")
+				.verifyComplete();
+	}
+
+	@Test
+	void createAFlux_fromIterable() {
+		List<String> fruitList = new ArrayList<>();
+		fruitList.add("Apple");
+		fruitList.add("Orange");
+		fruitList.add("Grape");
+		fruitList.add("Banana");
+		fruitList.add("Strawberry");
+
+		Flux<String> fruitFlux = Flux.fromIterable(fruitList);
+
+		StepVerifier.create(fruitFlux)
+				.expectNext("Apple")
+				.expectNext("Orange")
+				.expectNext("Grape")
+				.expectNext("Banana")
+				.expectNext("Strawberry")
+				.verifyComplete();
+	}
+
+	@Test
+	void createAFlux_fromStream() {
+		Stream<String> fruitStream = Stream.of("Apple", "Orange", "Grape", "Banana", "Strawberry");
+
+		Flux<String> fruitFlux = Flux.fromStream(fruitStream);
+
+		StepVerifier.create(fruitFlux)
+				.expectNext("Apple")
+				.expectNext("Orange")
+				.expectNext("Grape")
+				.expectNext("Banana")
+				.expectNext("Strawberry")
+				.verifyComplete();
+	}
+
+	@Test
+	void createAFlux_range() {
+		Flux<Integer> intervalFlux = Flux.range(1,5);
+
+		StepVerifier.create(intervalFlux)
+				.expectNext(1)
+				.expectNext(2)
+				.expectNext(3)
+				.expectNext(4)
+				.expectNext(5)
+				.verifyComplete();
+	}
+
+	@Test
+	void createAFlux_interval() {
+		Flux<Long> intervalFlux = Flux.interval(Duration.ofSeconds(1)).take(5);
+
+		StepVerifier.create(intervalFlux)
+				.expectNext(0L)
+				.expectNext(1L)
+				.expectNext(2L)
+				.expectNext(3L)
+				.expectNext(4L)
+				.verifyComplete();
+	}
+
+	@Test
+	void mergeFluxes() {
+		Flux<String> characterFlux = Flux.just("Garfield", "Kojak", "Barbossa")
+				.delayElements(Duration.ofMillis(500));
+
+		Flux<String> foodFlux = Flux.just("Lasagna", "Lollipops","Apples" )
+				.delaySubscription(Duration.ofMillis(250))
+				.delayElements(Duration.ofMillis(500));
+
+		Flux<String> mergedFlux = characterFlux.mergeWith(foodFlux);
+
+		StepVerifier.create(mergedFlux)
+				.expectNext("Garfield")
+				.expectNext("Lasagna")
+				.expectNext("Kojak")
+				.expectNext("Lollipops")
+				.expectNext("Barbossa")
+				.expectNext("Apples")
+				.verifyComplete();
+
+	}
+
+
+
+}
