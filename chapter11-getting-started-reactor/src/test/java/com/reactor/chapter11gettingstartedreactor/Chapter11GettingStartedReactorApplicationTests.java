c8649d3ce68118b7003c924d29783b6dc52da3d1
Phuong Nguyen <phuongsover2@gmail.com>
2024-01-21 17:46:16 +0700

    use flatMap() method

--- chapter11-getting-started-reactor/src/test/java/com/reactor/chapter11gettingstartedreactor/Chapter11GettingStartedReactorApplicationTests.java
+++ chapter11-getting-started-reactor/src/test/java/com/reactor/chapter11gettingstartedreactor/Chapter11GettingStartedReactorApplicationTests.java
@@ -4,11 +4,15 @@ import lombok.Data;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 import reactor.util.function.Tuple2;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.function.Consumer;
@@ -271,6 +275,31 @@ class Chapter11GettingStartedReactorApplicationTests {
 				.expectNext(new Player("Michael", "Jordan"))
 				.expectNext(new Player("Scottie", "Fippen"))
 				.expectNext(new Player("Steve", "Kerr"))
+				.verifyComplete();
+	}
+
+	@Test
+	public void flatMap() {
+		Flux<Player> playerFlux = Flux.just(
+				"Michael Jordan", "Scottie Pippen", "Steve Kerr"
+		)
+				.flatMap(n -> Mono.just(n)
+						.map(p -> {
+							String[] split = p.split("\\s");
+							return new Player(split[0], split[1]);
+						}))
+				.subscribeOn(Schedulers.parallel());
+
+		List<Player> playerList = Arrays.asList(
+				new Player("Michael", "Jordan"),
+				new Player("Scottie", "Pippen"),
+				new Player("Steve", "Kerr")
+		);
+
+		StepVerifier.create(playerFlux)
+				.expectNextMatches(p1 -> playerList.contains(p1))
+				.expectNextMatches(p2 -> playerList.contains(p2))
+				.expectNextMatches(p3 -> playerList.contains(p3))
 				.verifyComplete();
 	}
 
