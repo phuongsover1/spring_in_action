66210737dc1821904cc7053614a1952a5e062817
Phuong Nguyen <phuongsover2@gmail.com>
2024-01-21 15:17:00 +0700

    Provide Function to zip() that produces any object you'd like

--- chapter11-getting-started-reactor/src/test/java/com/reactor/chapter11gettingstartedreactor/Chapter11GettingStartedReactorApplicationTests.java
+++ chapter11-getting-started-reactor/src/test/java/com/reactor/chapter11gettingstartedreactor/Chapter11GettingStartedReactorApplicationTests.java
@@ -155,6 +155,18 @@ class Chapter11GettingStartedReactorApplicationTests {
     zippedFlux.subscribe(System.out::println);
 	}
 
+	@Test
+	public void zipFluxesToObject() {
+		Flux<String> characterFlux = Flux.just("Garfield", "Kojak", "Barbossa");
+		Flux<String> foodFlux = Flux.just("Lasagna", "Lollipops", "Apples");
 
+		Flux<String> zippedFlux = Flux.zip(characterFlux, foodFlux, (c,f) -> c + " eats " + f);
+
+		StepVerifier.create(zippedFlux)
+				.expectNext("Garfield eats Lasagna")
+				.expectNext("Kojak eats Lollipops")
+				.expectNext("Barbossa eats Apples")
+				.verifyComplete();
+	}
 
 }
