3afeb659cd86f864125ab8e0409b07f5fe9dbad5
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-12 23:28:59 +0700

    Sending resources through WebClient with base URI

--- chapter12-working-with-spring-WebFlux/src/main/java/tacos/web/api/consume/TacoConsumeController.java
+++ chapter12-working-with-spring-WebFlux/src/main/java/tacos/web/api/consume/TacoConsumeController.java
@@ -1,10 +1,14 @@
 package tacos.web.api.consume;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import tacos.domain.Taco;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping(path = "/api/consume/tacos", produces = "application/json")
@@ -18,7 +22,21 @@ public class TacoConsumeController {
         Mono<Taco> foundTaco = webClient.get().uri("/api/tacos/{id}", id)
                 .retrieve()
                 .bodyToMono(Taco.class);
-        return foundTaco;
+        return foundTaco.timeout(Duration.ofMillis(1))
+                .onErrorReturn(Taco.builder().id(-1L).name("DEFAULT TACO").build());
 
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public Mono<Taco> saveTacoOnExternalAPI(@RequestBody Mono<Taco> tacoMono) {
+        return webClient.post()
+                .uri("/api/tacos")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(tacoMono, Taco.class)
+                .retrieve()
+                .bodyToMono(Taco.class)
+                .timeout(Duration.ofSeconds(1))
+                .onErrorReturn(Taco.builder().id(-1L).name("DEFAULT TACO").build());
     }
 }
