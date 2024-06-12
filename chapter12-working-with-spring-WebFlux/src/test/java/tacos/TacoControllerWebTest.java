9ea3e73df8ec65efbce5b18693c74051e86339d6
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-12 22:29:26 +0700

    GETting resources through WebClient

--- chapter12-working-with-spring-WebFlux/src/test/java/tacos/TacoControllerWebTest.java
+++ chapter12-working-with-spring-WebFlux/src/test/java/tacos/TacoControllerWebTest.java
@@ -1,6 +1,7 @@
 package tacos;
 
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +9,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import tacos.domain.Ingredient;
+import tacos.domain.Taco;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TacoControllerWebTest {
     @Autowired
     private WebTestClient webTestClient;
+
 
     @Test
     public void shouldReturnRecentTacos() {
@@ -27,5 +33,19 @@ public class TacoControllerWebTest {
                 .jsonPath("$[?(@.name == 'Taco1')]").exists()
                 .jsonPath("$[?(@.name == 'Taco2')]").exists()
                 .jsonPath("$[?(@.name == 'Taco3')]").exists();
+    }
+
+    @Test
+    public void consumeTaco() {
+        Mono<Taco> tacoMono = WebClient.create()
+                .get()
+                .uri("http://localhost:8080/api/tacos/{id}", 1L)
+                .retrieve()
+                .bodyToMono(Taco.class);
+
+        tacoMono.subscribe(i -> {
+            Assertions.assertEquals("Taco1", i.getName());
+            Assertions.assertEquals(1L, i.getId());
+        });
     }
 }
