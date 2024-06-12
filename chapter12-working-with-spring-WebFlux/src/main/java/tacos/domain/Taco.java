3cef283b448257faf93dadb6a29be14334d1992d
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-12 08:51:58 +0700

    Testing reactive TacoController's GET request, and validate body using jsonPath

--- chapter12-working-with-spring-WebFlux/src/main/java/tacos/domain/Taco.java
+++ chapter12-working-with-spring-WebFlux/src/main/java/tacos/domain/Taco.java
@@ -12,11 +12,13 @@ import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties("ingredients")
